cfa50316783c1f418a667458a6674cf9d1ddc78d
dilshodbek <dilshodbeka404@gmail.com>
2023-02-23 19:13:12 +0500

    Initial commit

---
+++ src/main/java/uz/uzkassa/TaskUzKassaApplication.java
@@ -0,0 +1,13 @@
+package uz.uzkassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskUzKassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskUzKassaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/uzkassa/TaskUzKassaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.uzkassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskUzKassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
